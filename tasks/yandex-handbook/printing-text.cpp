a054ad1dc2f72cd17ad0781bb3e54de4e462d5cf
Егор Лунёв <egorrshuk@gmail.com>
2026-09-07 17:46:08 +0300

    init

---
+++ tasks/yandex-handbook/printing-text.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "C++ is a general-purpose programming language with a bias towards systems programming that\n\t- is a better C\n\t- supports data abstraction\n\t- supports object-oriented programming\n\t- supports generic programming.";
+
+    return 0;
+}
