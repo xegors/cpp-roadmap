bc9498141e93eccbc40e91ba36fdb301dfdb1f8c
xegors <egorrshuk@gmail.com>
2026-09-17 16:18:19 +0300

    fix: commonPrefix working now!

--- tasks/yandex-handbook/functions/commonPrefix.cpp
+++ tasks/yandex-handbook/functions/commonPrefix.cpp
@@ -2,15 +2,18 @@
 #include <vector>
 
 std::string CommonPrefix(const std::vector<std::string>& words) {
-    std::string res;
+	if (words.empty()){
+		return "";
+	}
 
     for (size_t i = 0; i < words[0].size(); ++i) {
-        res = words[0].substr(0, i);
+		char cur_char = words[0][i];
+		
         for (const std::string& word : words) {
-            if (res != word.substr(0, i))
-                return res.substr(0, i-1);
-        }
-    }
+			if (i >= word.size() || word[i] != cur_char)
+				return words[0].substr(0, i);
+		}
+	}
 
-    return res;
+    return words[0];
 }
